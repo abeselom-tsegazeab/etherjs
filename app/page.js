14f00e04ce229cf321cc6852d3b5b83169cfd29f
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2023-12-29 21:50:11 +0300

    balance

--- app/page.js
+++ app/page.js
@@ -1,16 +1,18 @@
 "use client";
 import { ethers } from "ethers";
-import React, {  useEffect } from "react";
+import React, {  useEffect, useLayoutEffect } from "react";
 export default function Home() {
   const address = "0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5";
   const etherjs = async () => {
     const provider = new ethers.providers.EtherscanProvider();
 
     const balance = await provider.getBalance(address);
-    console.log(balance);
-
-    console.log(provider);
+    const showBalance =await ethers.utils.formatEther(balance)
+    console.log(showBalance);
   };
+ useEffect(()=>{
+  etherjs()
+ },[])
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Hi there</div>
