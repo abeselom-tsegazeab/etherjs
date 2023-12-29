9cdcb8ba2f3f9b9c7bbcfc04db29fbe3873f2b8c
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2023-12-29 21:45:05 +0300

    ether provider

--- app/page.js
+++ app/page.js
@@ -1,20 +1,19 @@
-'use client'
-import Image from 'next/image'
-import { ethers } from 'ethers'
-import React,{useState,useEffect} from 'react'
+"use client";
+import { ethers } from "ethers";
+import React, {  useEffect } from "react";
 export default function Home() {
-  const [currentAccount,setCurrentAccount] = useState("")
-  const address = "0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5"
-  const balance = provider.getBalance()
+  const address = "0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5";
+  const etherjs = async () => {
+    const provider = new ethers.providers.EtherscanProvider();
 
-  const provider = new ethers.providers.EtherscanProvider()
-  console.log(provider)
+    const balance = await provider.getBalance(address);
+    console.log(balance);
+
+    console.log(provider);
+  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
-    <div>
-      Hi there
-    </div>
+      <div>Hi there</div>
     </main>
-  )
+  );
 }
