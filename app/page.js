bdbbc6a3888b9e46c7ae4992abb6892ac96cfc6d
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2023-12-29 22:16:07 +0300

    transaction

--- app/page.js
+++ app/page.js
@@ -1,16 +1,18 @@
 "use client";
 import { ethers } from "ethers";
-import React, {  useEffect, useLayoutEffect } from "react";
+import React, {  useEffect, useLayoutEffect, useState } from "react";
 export default function Home() {
   const address = "0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5";
+  const [transaction,setTransaction] = useState([]);
   const etherjs = async () => {
     const provider = new ethers.providers.EtherscanProvider();
 
     const balance = await provider.getBalance(address);
     const showBalance =await ethers.utils.formatEther(balance)
-    const checkFunction = await provider.getAvatar()
-    
-    console.log(showBalance);
+    const checkFunction = await provider.getBlock(18893205)
+    const number = checkFunction.transactions
+    setTransaction(number)
+    console.log(number);
   };
  useEffect(()=>{
   etherjs()
@@ -18,6 +20,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Hi there</div>
+      {transaction.map((tsx,index)=>(
+        <div key={index}>
+          <h6 className="text-lg leading-10">{tsx}</h6>
+        </div>
+      ))}
     </main>
   );
 }
