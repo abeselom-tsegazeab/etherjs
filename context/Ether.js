15dd96d518d23e91e9279f32be1d6456a391f1a5
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2024-01-02 14:31:53 +0300

    block tsx

--- context/Ether.js
+++ context/Ether.js
@@ -1,26 +1,46 @@
-'use client'
-import React, { useContext, useState } from "react";
+"use client";
+import React, { useContext, useEffect, useState } from "react";
 import { ethers } from "ethers";
 
 const provider = new ethers.providers.JsonRpcProvider(
-    `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_API_KEY}`
-)
-export const Etherscan = React.createContext()
+  `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_API_KEY}`
+);
+export const Etherscan = React.createContext();
 
-export const EtherProvider = ({children})=>{
-    const [data, setData] = useState('Etherscan Clone')
+export const EtherProvider = ({ children }) => {
+  const [data, setData] = useState("Etherscan Clone");
+  const tenBlockWithDetails = [];
+  const [yourBlockTsx, setYourBlockTsx] = useState(tenBlockWithDetails);
+  const [currentBlock, setCurrentBlock] = useState([]);
+  const [topTenBlock, setTopTenBlock] = useState([]);
+  const [transaction, setTransaction] = useState([]);
+  const [gasPrice, setGasPrice] = useState("");
 
-    return (
-        <Etherscan.Provider value={{data}}>
-            <div>
-            {children}
-            </div>
-        </Etherscan.Provider> 
-    )
+    const accountDetails = async ()=>{
+        try {
+            const getCurrentBlock = await provider.getBlockNumber();
+            setCurrentBlock(getCurrentBlock)
+
+            // get single block transaction
+            // getBlock gives the data in the current block
+            const blockTransaction = await provider.getBlock(getCurrentBlock)
+            setYourBlockTsx(blockTransaction)
+        } catch (error) {
+            console.log('Something went wrong while fetching data',error)
+        }
+    }
+    useEffect(() => {
+        accountDetails()
+
+}, [])
+
+  return (
+    <Etherscan.Provider value={{ data }}>
+      <div>{children}</div>
+    </Etherscan.Provider>
+  );
+};
+
+export function useEtherScan() {
+  return useContext(Etherscan);
 }
-
-export function useEtherScan(){
-    return useContext(Etherscan)
-}
-
-
