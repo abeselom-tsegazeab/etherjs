7be21ff0d527a157766f3b78171d0cefea9cb813
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2024-01-02 14:41:47 +0300

    get block details

--- context/Ether.js
+++ context/Ether.js
@@ -16,23 +16,40 @@ export const EtherProvider = ({ children }) => {
   const [transaction, setTransaction] = useState([]);
   const [gasPrice, setGasPrice] = useState("");
 
-    const accountDetails = async ()=>{
-        try {
-            const getCurrentBlock = await provider.getBlockNumber();
-            setCurrentBlock(getCurrentBlock)
+  const accountDetails = async () => {
+    try {
+      const getCurrentBlock = await provider.getBlockNumber();
+      setCurrentBlock(getCurrentBlock);
 
-            // get single block transaction
-            // getBlock gives the data in the current block
-            const blockTransaction = await provider.getBlock(getCurrentBlock)
-            setYourBlockTsx(blockTransaction)
-        } catch (error) {
-            console.log('Something went wrong while fetching data',error)
-        }
+      // get single block transaction
+      // getBlock gives the data in the current block
+      const blockTransaction = await provider.getBlock(getCurrentBlock);
+      setTransaction(blockTransaction.transactions);
+
+      // top ten blocks
+      const previosBlock = getCurrentBlock - 10;
+      const listTenBlock = []
+
+      for (let i = getCurrentBlock;i > previosBlock;i--){
+        listTenBlock.push([i])
+      }
+
+      // get block details
+      const getBlockDetails = listTenBlock.flat();
+      setTopTenBlock(getBlockDetails);
+      
+      getBlockDetails.map(async (e)=>{
+        const singleBlockData = await provider.getBlock(e);
+        tenBlockWithDetails.push([singleBlockData])
+
+      })
+    } catch (error) {
+      console.log("Something went wrong while fetching data", error);
     }
-    useEffect(() => {
-        accountDetails()
-
-}, [])
+  };
+  useEffect(() => {
+    accountDetails();
+  }, []);
 
   return (
     <Etherscan.Provider value={{ data }}>
