3d980952d7a1344668b9bd200d40765d76c9c2a7
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2024-01-02 23:34:03 +0300

    block transactions

--- app/page.js
+++ app/page.js
@@ -13,23 +13,22 @@ import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-  const { data, yourBlockTsx } = useEtherScan();
+  const { data, yourBlockTsx, transaction } = useEtherScan();
   const [userAccount, setUserAccount] = useState("");
 
   // convert ether
-  const convertIntoEth = (amount)=>{
-    const ETH = ethers.utils.formatUnits(amount,'ether');
+  const convertIntoEth = (amount) => {
+    const ETH = ethers.utils.formatUnits(amount, "ether");
     return ETH;
-  }
-
+  };
   // input address
-  const accountAddress = (e)=>{
-    e.preventDefault()
-    let address = document.getElementById("accountAddress").value.trim()
-    setUserAccount(address)
-    router.push(`/account?${address}`)
-    address = '';
-  }
+  const accountAddress = (e) => {
+    e.preventDefault();
+    let address = document.getElementById("accountAddress").value.trim();
+    setUserAccount(address);
+    router.push(`/account?${address}`);
+    address = "";
+  };
 
   return (
     <main className="">
@@ -42,7 +41,10 @@ export default function Home() {
               placeholder="Ether Account address"
               id="accountAddress"
             />
-            <Link href={{ pathname: "/account", query: userAccount }} onClick={e=>accountAddress(e)}>
+            <Link
+              href={{ pathname: "/account", query: userAccount }}
+              onClick={(e) => accountAddress(e)}
+            >
               <SiMinutemailer />
             </Link>
           </form>
@@ -57,11 +59,13 @@ export default function Home() {
                 <div className={Style.oneBlock} key={i}>
                   <div className={Style.block}>
                     <div className={Style.info}>
-                      <div className={Style.bk}>
-                        <p>BK</p>
-                        <Link href={{ pathname: "/block", query: e.number }}>
-                          {e[0].number}
-                        </Link>
+                      <div className={`${Style.bk} !w-full`}>
+                        <div className="">
+                          <p className="w-full">BK No</p>
+                          <Link href={{ pathname: "/block", query: e.number }}>
+                            {e[0].number}
+                          </Link>
+                        </div>
                       </div>
                       <p>{e[0].timestamp}</p>
                     </div>
@@ -86,11 +90,32 @@ export default function Home() {
                           &nbsp;TSX in 3 Sec
                         </span>
                       </div>
-                    <div className={Style.reward}>
-                      <p>{convertIntoEth(e.baseeFeePerGas)} <span>ETH</span></p>
-                      <Image src={logo} className={Style.eth} alt='Ether Logo' width={10} height={10}/>
+                      <div className={Style.reward}>
+                        <p>
+                          {convertIntoEth(e[0].baseFeePerGas._hex)}{" "}
+                          <span>ETH</span>
+                        </p>
+                        {/* <Image src={logo} className={Style.eth} alt='Ether Logo' width={10} height={10}/> */}
+                      </div>
                     </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          {/* Transactions */}
+          <div className={Style.container_box}>
+            <h3>Latest Transaction</h3>
+            <div className={Style.container_block}>
+              {transaction.map((e, i) => (
+                <div className={Style.oneBlock} key={i}>
+                  <div className={Style.info}>
+                    <div>
+                      <p className={Style.bx}>TNX</p>
                     </div>
+                    <Link href={{ pathname: "/transaction", query: e }}>
+                      Hash: &nbsp;&nbsp;{e.slice(0, 55)}...
+                    </Link>
                   </div>
                 </div>
               ))}
