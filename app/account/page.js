1aabe8276e4a565398aaf2ae4ef2ce5e46041ffb
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2024-01-04 18:17:51 +0300

    router

--- app/account/page.js
+++ app/account/page.js
@@ -1,73 +1,94 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { useRouter } from 'next/router'
-import { ethers } from 'ethers'
-import  Image from 'next/image'
+import React, { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { ethers } from "ethers";
+import Image from "next/image";
 
-
-
-import Style from '../../styles/Account.module.css'
-import { useEtherScan } from '@/context/Ether'
-
+import Style from "../../styles/Account.module.css";
+import { useEtherScan } from "@/context/Ether";
+import Loading from "@/components/Loading";
 
 const page = () => {
-  const {provider} = useEtherScan()
-  const router = useRouter()
-  const {query} = router
+  const { provider } = useEtherScan();
+  const router = useRouter();
+  const { query } = router;
   const acc = Object.keys(query)[0];
 
-  const [account, setAccount] = useState('')
-  const [balance, setBalance] = useState('')
-  const [totalTransaction, setTotalTransaction] = useState('')
-  const [name, setName] = useState('')
-  const [open, setOpen] = useState(true)
+  const [account, setAccount] = useState("");
+  const [balance, setBalance] = useState("");
+  const [totalTransaction, setTotalTransaction] = useState("");
+  const [name, setName] = useState("");
+  const [open, setOpen] = useState(true);
   const [loading, setLoading] = useState(true);
 
-  const [accountHistory, setAccountHistory] = useState([])
-  const [internalByAddress, setInternalByAddress] = useState([])
-  const [ERC20, setERC20] = useState([])
-  const [ERC21, setERC21] = useState([])
-  const [ERC1155, setERC1155] = useState([])
-  const [blockMindedByAddress, setBlockMindedByAddress] = useState([])
-  const [blockRangeTransaction, setBlockRangeTransaction] = useState([])
+  const [accountHistory, setAccountHistory] = useState([]);
+  const [internalByAddress, setInternalByAddress] = useState([]);
+  const [ERC20, setERC20] = useState([]);
+  const [ERC21, setERC21] = useState([]);
+  const [ERC1155, setERC1155] = useState([]);
+  const [blockMindedByAddress, setBlockMindedByAddress] = useState([]);
+  const [blockRangeTransaction, setBlockRangeTransaction] = useState([]);
 
-
-  const accountData = async ()=>{
+  const accountData = async () => {
     try {
-      setAccount(acc)
+      setAccount(acc);
 
-      if(open){
-        setOpen(false)
+      if (open) {
+        setOpen(false);
       }
 
       // account name
-      const ESN = await provider.lookupAddress(acc)
+      const ESN = await provider.lookupAddress(acc);
 
-      if(ESN === null){
+      if (ESN === null) {
         setName(ESN);
-        setLoading(false)
-      }else{
-        setName(ESN)
-        setLoading(true)
+        setLoading(false);
+      } else {
+        setName(ESN);
+        setLoading(true);
       }
     } catch (error) {
-      console.log('Something went wrong')
+      console.log("Something went wrong");
     }
-  }
+  };
   return (
     <div className={Style.accountDIV}>
-{open? (
-  <div className={Style.btnContainer}>
-    <h1>
-      {open ? "Welcome To Ether Finance":"Please wait we are loading data"}
-    </h1>
-    <button className={Style.openBtn} onClick={()=>accountData()}>
-    
-    </button>
-  </div>
-):('')}
+      {open ? (
+        <div className={Style.btnContainer}>
+          <h1>
+            {open
+              ? "Welcome To Ether Finance"
+              : "Please wait we are loading data"}
+          </h1>
+          <button
+            className={Style.openBtn}
+            onClick={() => accountData()}
+          ></button>
+        </div>
+      ) : (
+      <div>
+         {loading ? (<div className={Style.loading}>
+          <Loading/>
+        </div>):(
+          <div className={Style.container}>
+            <div className={Style.box}>
+              <div className={Style.account}>
+                <Image src={logo} alt="logo" width={50} height={50}/>
+                <p>
+                  Address:<span>{acc}</span>
+                </p>
+              </div>
+              <div className={Style.owner}>
+                <p onClick={()=>accountData()}>Owner</p>
+                {name || "Hello"}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
