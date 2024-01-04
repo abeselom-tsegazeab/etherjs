e26710a3bbc4e23eb398c335798a8ccf325ffdb3
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2024-01-04 13:27:27 +0300

    router

--- app/account/page.js
+++ app/account/page.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import { ethers } from 'ethers'
+import  Image from 'next/image'
+
+
 
 const page = () => {
   return (
