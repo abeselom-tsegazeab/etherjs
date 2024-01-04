ba5d9435135ceb0b518fdd3468e2eda241d7914e
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2024-01-04 17:27:48 +0300

    block minded transactions

--- app/account/page.js
+++ app/account/page.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/router'
 import { ethers } from 'ethers'
@@ -7,7 +7,7 @@ import  Image from 'next/image'
 
 
 import Style from '../../styles/Account.module.css'
-import { Etherscan, useEtherScan } from '@/context/Ether'
+import { useEtherScan } from '@/context/Ether'
 
 
 const page = () => {
@@ -19,7 +19,19 @@ const page = () => {
   const [account, setAccount] = useState('')
   const [balance, setBalance] = useState('')
   const [totalTransaction, setTotalTransaction] = useState('')
+  const [name, setName] = useState('')
+  const [open, setOpen] = useState(true)
+  const [loading, setLoading] = useState(true);
+
+  const [accountHistory, setAccountHistory] = useState([])
+  const [internalByAddress, setInternalByAddress] = useState([])
+  const [ERC20, setERC20] = useState([])
+  const [ERC21, setERC21] = useState([])
+  const [ERC1155, setERC1155] = useState([])
+  const [blockMindedByAddress, setBlockMindedByAddress] = useState([])
+  const [blockRangeTransaction, setBlockRangeTransaction] = useState([])
   
+
   return (
     <div>Acccount</div>
   )
