6038b5c2d0a7b100583a92cae0c530e679f6e8b5
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2024-01-04 13:37:44 +0300

    loading

---
+++ components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Style from '../styles/Loading.module.css'
+const Loading = () => {
+  return (
+    <section class={Style.sec-loading}>
+  <div class={Style.one}>
+  </div>
+</section>
+  )
+}
+
+export default Loading
