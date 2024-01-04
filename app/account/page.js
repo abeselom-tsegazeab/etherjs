7f4352905df6bb18290baa5e2974d6b14afe3051
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2024-01-04 16:59:57 +0300

    router

--- app/account/page.js
+++ app/account/page.js
@@ -7,10 +7,19 @@ import  Image from 'next/image'
 
 
 import Style from '../../styles/Account.module.css'
-import { Etherscan } from '@/context/Ether'
+import { Etherscan, useEtherScan } from '@/context/Ether'
 
 
 const page = () => {
+  const {provider} = useEtherScan()
+  const router = useRouter()
+  const {query} = router
+  const acc = Object.keys(query)[0];
+
+  const [account, setAccount] = useState('')
+  const [balance, setBalance] = useState('')
+  const [totalTransaction, setTotalTransaction] = useState('')
+  
   return (
     <div>Acccount</div>
   )
