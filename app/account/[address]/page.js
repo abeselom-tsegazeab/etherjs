ab3309d4e4573a566dee71d95b1cf4f6697d5a29
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2024-01-05 13:43:37 +0300

    block

--- app/account/[address]/page.js
+++ app/account/[address]/page.js
@@ -62,17 +62,9 @@ const page = () => {
     setInternalByAddress(tsxByInternalHash.data.result)
 
    // etherscan api erc20 token
-   const ERC20Token = await axios.get(`https://api.etherscan.io/api
-   ?module=account
-   &action=tokentx
-   &contractaddress=0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2
-   &address=0x4e83362442b8d1bec281594cea3050c8eb01311c
-   &page=1
-   &offset=100
-   &startblock=0
-   &endblock=27025780
-   &sort=asc
-   &apikey=${process.env.NEXT_PUBLIC_ETHER_API_KEY}`)
+  //  const ERC20Token = await axios.get(`https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2&address=0x4e83362442b8d1bec281594cea3050c8eb01311c&page=1&offset=100&startblock=0
+  //  &endblock=27025780&sort=asc&apikey=${process.env.NEXT_PUBLIC_ETHER_API_KEY}`)
+  //  console.log(ERC20Token)
     } catch (error) {
       console.log(error);
     }
