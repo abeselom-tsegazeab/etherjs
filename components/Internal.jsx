08c9d3dce6ee81c6696f13be8d40de07f73620d8
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2024-01-08 21:57:34 +0300

    hash

--- components/Internal.jsx
+++ components/Internal.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import { FaFilter } from 'react-icons/fa'
+import { AiFillEye } from 'react-icons/ai'
+import Link from 'next/link'
 
-const Internal = () => {
+
+import Style from '@/styles/Table.module.css'
+
+
+
+const Internal = ({internalByAddress,handleClick}) => {
   return (
-    <div>Internal</div>
+    <div className={Style.dataTable}>
+      <div className={Style.column}>
+        <div className={Style.tableTitle}>
+          <p>Hash</p>
+        </div>
+        <div className={Style.tableInfo}>
+          {internalByAddress.map((e,i)=>(
+            <div className={Style.transHash} key={i}>
+              <AiFillEye/>
+              <p>{e.hash.slice(0,35)}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
