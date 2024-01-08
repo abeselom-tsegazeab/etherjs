2806d74f1952f8c3632a7fffc501c37b9d48ad9c
abeselom-tsegazeab <a.abeselom.t@gmail.com>
2024-01-09 00:39:15 +0300

    block

--- components/BlockRange.jsx
+++ components/BlockRange.jsx
@@ -2,9 +2,69 @@ import React, { useEffect, useState } from "react";
 import { AiFillEye } from "react-icons/ai";
 import Link from "next/link";
 
-const BlockRange = () => {
+const BlockRange = ({blockRangeTransaction, handleClick}) => {
   return (
-    <div>BlockRange</div>
+    <div>
+      {blockRangeTransaction.length === 0 ? (
+        <div className={Style.sorry}>
+          <p>Sorry There is no data</p>
+        </div>
+      ) : (
+        <div className={Style.dataTable}>
+          {/* Block No */}
+          <div className={Style.column}>
+            <div className={Style.tableTable}>
+              <p>Block Number</p>
+            </div>
+            <div className={Style.tableInfo}>
+              {blockRangeTransaction.map((e, i) => (
+                <div className={Style.transHash} key={i}>
+                  <AiFillEye />
+                  <p className={Style.toLink}>
+                    <Link href={{ pathname: `/block/${e.blockNumber}` }}>
+                      {e.blockNumber}
+                    </Link>
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* Block Reward */}
+          <div className={Style.column}>
+            <div className={Style.tableTable}>
+              <p>Block Reward</p>
+            </div>
+            <div className={Style.tableInfo}>
+              {blockRangeTransaction.map((e, i) => (
+                <div className={Style.transHash} key={i}>
+                  <p>{e.blockReward.slice(0, 10)}...</p>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* Timestamp */}
+          <div className={Style.column}>
+            <div className={Style.tableTable}>
+              <p>TimeStamp</p>
+            </div>
+            <div className={Style.tableInfo}>
+              {blockRangeTransaction.map((e, i) => (
+                <div className={Style.transHash} key={i}>
+                  <p>{e.timeStamp.slice(0, 10)}...</p>
+                </div>
+              ))}
+            </div>
+          </div>
+
+
+
+
+
+        </div>
+      )}
+    </div>
   )
 }
 
